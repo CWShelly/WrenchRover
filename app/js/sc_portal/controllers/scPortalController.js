12f3c072c49864fa3b74ddbb8b0f3cd3cf9b7f75
Shelly Yusuf <ShellyNYusuf@gmail.com>
2017-04-02 23:26:41 -0700

    piping requests on scportal

--- app/js/sc_portal/controllers/scPortalController.js
+++ app/js/sc_portal/controllers/scPortalController.js
@@ -7,8 +7,9 @@ module.exports = function(app) {
     this.welcome = localStorage.getItem('service_center_name');
     console.log(this.welcome);
     this.newRequests = [];
-    this.di = [];
+    this.arr = [];
     this.workrequests = [];
+    this.empty = '';
 
 
     this.getRequests = function() {
@@ -24,23 +25,55 @@ module.exports = function(app) {
         for (var i = 0; i < res.data.length; i++) {
           if (res.data[i].work_request.indexOf('[') != -1) {
 
-            console.log(res.data[i].work_request[0]);
             if (res.data[i].work_request[0] != '[') {
               res.data[i].describe_issue = res.data[i].work_request.slice(0, res.data[i].work_request.indexOf('[') - 1);
             }
+            res.data[i].parstring = '';
+            // res.data[i].arr = [];
+
+
+            console.log(res.data[i].arr);
 
             res.data[i].array_request =
-
           res.data[i].work_request.slice(res.data[i].work_request.indexOf('[') + 1,
-            res.data[i].work_request.indexOf(']') - 1);
+            res.data[i].work_request.indexOf(']') - 1)
+            ;
+            console.log(res.data[i].array_request);
+            res.data[i].parstring = '';
 
+            res.data[i].parstring = rq( res.data[i].work_request.slice(res.data[i].work_request.indexOf('[') + 1,
+                    res.data[i].work_request.indexOf(']') - 1), this.empty);
+            console.log('x');
+
+
+            res.data[i].piped = res.data[i].parstring.replace(/,/g, ' | ');
+
+// ....
             this.workrequests.push(res.data[i]);
           }
+
         }
 
-        console.log(res.data);
-        // console.log(res.data[i].array_request[0]);
+        function two(arr) {
+          for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == ',') {
+              arr[i] = ' | ';
+            }
+          }
+          return arr.join();
+        }
 
+        function rq(str, str2) {
+          for (var i = 0; i < str.length; i++) {
+            if (str[i] != '"') {
+              str2 += str[i];
+            }
+          }
+          return str2;
+        }
+
+
+        console.log(res.data);
         console.log(this.workrequests);
 
       });};
