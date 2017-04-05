a42cc636762b327b83aaf065e1dcdc7b156a5201
Shelly Yusuf <ShellyNYusuf@gmail.com>
2017-04-05 16:06:28 -0700

    toggling the lights

--- app/js/describe/controllers/describe_controller.js
+++ app/js/describe/controllers/describe_controller.js
@@ -77,7 +77,7 @@ module.exports = function(app) {
     this.button = 'Enter';
     this.button = that.button;
     this.dashArr2 = [];
-    this.dashChild = [];
+    // this.dashChild = [];
     this.mainLights = [];
 
     this.commonLight = {
@@ -87,45 +87,44 @@ module.exports = function(app) {
     this.mainLights.push(this.commonLight);
 
 
-    this.sbArr = [];
+    // this.sbArr = [];
+    //
+    // this.sb = {
+    //   children: [
+    //     { imageSrc: '../../../images/dashlights/001.png',
+    //       imageSrc2: '../../../images/dashlights/002.png',
+    //       imageSrc3: '../../../images/dashlights/003.png',
+    //       imageSrc4: '../../../images/dashlights/004.png',
+    //       name: 'Oil Light',
+    //       name2: 'Battery Light',
+    //       name3: 'Encircled Exclamation Point',
+    //       name4: 'Airbag Light'
+    //     },
+    //     { imageSrc: '../../../images/dashlights/005.png',
+    //       imageSrc2: '../../../images/dashlights/010.png',
+    //       imageSrc3: '../../../images/dashlights/011.png',
+    //       imageSrc4: '../../../images/dashlights/012.png',
+    //       name: 'Temperature Light',
+    //       name2: 'ABS Light',
+    //       name3: 'Check Engine Light',
+    //       name4: 'Sprocket Light'
+    //     },
+    //     { imageSrc: '../../../images/dashlights/015.png',
+    //       imageSrc2: '../../../images/dashlights/016.png',
+    //       imageSrc3: '../../../images/dashlights/017.png',
+    //       imageSrc4: '../../../images/dashlights/018.png',
+    //       name: 'Coily Light',
+    //       name2: 'Hazard Light',
+    //       name3: 'Tire Pressure Light',
+    //       name4: 'Lightbulb Light'
+    //     }
+    //   ]
+    // };
 
-
-    this.sb = {
-      children: [
-        { imageSrc: '../../../images/dashlights/001.png',
-          imageSrc2: '../../../images/dashlights/002.png',
-          imageSrc3: '../../../images/dashlights/003.png',
-          imageSrc4: '../../../images/dashlights/004.png',
-          name: 'Oil Light',
-          name2: 'Battery Light',
-          name3: 'Encircled Exclamation Point',
-          name4: 'Airbag Light'
-        },
-        { imageSrc: '../../../images/dashlights/005.png',
-          imageSrc2: '../../../images/dashlights/010.png',
-          imageSrc3: '../../../images/dashlights/011.png',
-          imageSrc4: '../../../images/dashlights/012.png',
-          name: 'Temperature Light',
-          name2: 'ABS Light',
-          name3: 'Check Engine Light',
-          name4: 'Sprocket Light'
-        },
-        { imageSrc: '../../../images/dashlights/015.png',
-          imageSrc2: '../../../images/dashlights/016.png',
-          imageSrc3: '../../../images/dashlights/017.png',
-          imageSrc4: '../../../images/dashlights/018.png',
-          name: 'Coily Light',
-          name2: 'Hazard Light',
-          name3: 'Tire Pressure Light',
-          name4: 'Lightbulb Light'
-        }
-      ]
-    };
-
-    this.sbArr.push(this.sb);
-    for (var i = 0; i < this.sbArr[0].children.length; i++) {
-      this.dashChild.push(this.sbArr[0].children[i]);
-    }
+    // this.sbArr.push(this.sb);
+    // for (var i = 0; i < this.sbArr[0].children.length; i++) {
+    //   this.dashChild.push(this.sbArr[0].children[i]);
+    // }
 
 
     this.goTo = function() {
